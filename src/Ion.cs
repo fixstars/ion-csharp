cb780501c86b0650d6cbce156fa3965f99bb790b
Takuro Iizuka <t_iizuka@fixstars.com>
2020-11-20 18:59:53 -0800

    Test simple case

--- src/Ion.cs
+++ src/Ion.cs
@@ -14,9 +14,9 @@ public class Ion
     [StructLayout(LayoutKind.Sequential)]
     public struct Type
     {
-        TypeCode code;
-        Byte bits;
-        Byte lanes;
+        public TypeCode code;
+        public Byte bits;
+        public Byte lanes;
 
         public Type(TypeCode code_, Byte bits_, Byte lanes_)
         {
@@ -29,13 +29,13 @@ public class Ion
     [StructLayout(LayoutKind.Sequential)]
     public struct BuilderCompileOption
     {
-        string output_directory;
+        public string output_directory;
     }
 
     private static class NativeMethods
     {
         [DllImport("ion-core.dll")]
-        internal static extern Int32 ion_port_create(IntPtr ptr, String key, Type type);
+        internal static extern Int32 ion_port_create(IntPtr ptr, String key, Type type, Int32 dim);
 
         [DllImport("ion-core.dll")]
         internal static extern Int32 ion_port_destroy(IntPtr obj);
@@ -56,10 +56,10 @@ public class Ion
         internal extern static Int32 ion_node_get_port(IntPtr obj, String key, IntPtr port_ptr);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_node_set_port(IntPtr obj, IntPtr ports_ptr, Int32);
+        internal extern static Int32 ion_node_set_port(IntPtr obj, IntPtr ports_ptr, Int32 ports_num);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_node_set_param(IntPtr obj, IntPtr params_ptr, Int32);
+        internal extern static Int32 ion_node_set_param(IntPtr obj, IntPtr params_ptr, Int32 params_num);
 
         [DllImport("ion-core.dll")]
         internal extern static Int32 ion_builder_create(IntPtr ptr);
@@ -74,10 +74,10 @@ public class Ion
         internal extern static Int32 ion_builder_with_bb_module(IntPtr obj, String module_name);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_builder_add_node(IntPtr obj, String key, IntPtr node_ptr)
+        internal extern static Int32 ion_builder_add_node(IntPtr obj, String key, IntPtr node_ptr);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_builder_compile(IntPtr obj, String function_name, IntPtr option);
+        internal extern static Int32 ion_builder_compile(IntPtr obj, String function_name, BuilderCompileOption option);
 
         [DllImport("ion-core.dll")]
         internal extern static Int32 ion_builder_save(IntPtr obj, String file_name);
@@ -110,59 +110,59 @@ public class Ion
         internal extern static Int32 ion_port_map_destroy(IntPtr obj);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_i8(IntPtr obj, ion_port_t, SByte);
+        internal extern static Int32 ion_port_map_set_i8(IntPtr obj, IntPtr p, SByte v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_i16(IntPtr obj, IntPtr p, Int16);
+        internal extern static Int32 ion_port_map_set_i16(IntPtr obj, IntPtr p, Int16 v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_i32(IntPtr obj, IntPtr p, Int32);
+        internal extern static Int32 ion_port_map_set_i32(IntPtr obj, IntPtr p, Int32 v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_i64(IntPtr obj, IntPtr p, Int64);
+        internal extern static Int32 ion_port_map_set_i64(IntPtr obj, IntPtr p, Int64 v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_u1(IntPtr obj, IntPtr p, bool);
+        internal extern static Int32 ion_port_map_set_u1(IntPtr obj, IntPtr p, bool v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_u8(IntPtr obj, IntPtr p, Byte);
+        internal extern static Int32 ion_port_map_set_u8(IntPtr obj, IntPtr p, Byte v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_u16(IntPtr obj, IntPtr p, UInt16);
+        internal extern static Int32 ion_port_map_set_u16(IntPtr obj, IntPtr p, UInt16 v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_u32(IntPtr obj, IntPtr p, UInt32);
+        internal extern static Int32 ion_port_map_set_u32(IntPtr obj, IntPtr p, UInt32 v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_u64(IntPtr obj, IntPtr p, UInt64);
+        internal extern static Int32 ion_port_map_set_u64(IntPtr obj, IntPtr p, UInt64 v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_f32(IntPtr obj, IntPtr p, float);
+        internal extern static Int32 ion_port_map_set_f32(IntPtr obj, IntPtr p, float v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_f64(IntPtr obj, IntPtr p, double);
+        internal extern static Int32 ion_port_map_set_f64(IntPtr obj, IntPtr p, double v);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_buffer(IntPtr obj, IntPtr p, ion_buffer_t);
+        internal extern static Int32 ion_port_map_set_buffer(IntPtr obj, IntPtr p, IntPtr buffer);
 
         [DllImport("ion-core.dll")]
-        internal extern static Int32 ion_port_map_set_buffer_array(IntPtr obj, IntPtr p, ion_buffer_t *, int);
+        internal extern static Int32 ion_port_map_set_buffer_array(IntPtr obj, IntPtr p, IntPtr buffers, Int32 buffers_num);
 
     }
 
     public class Port
     {
-        private IntPtr obj;
+        public IntPtr obj;
 
         public Port(IntPtr obj_)
         {
-            obj = obj_
+            obj = obj_;
         }
 
-        public Port(String key, Type type)
+        public Port(String key, Type type, Int32 dim)
         {
             IntPtr ptr = Marshal.AllocHGlobal(IntPtr.Size);
-            Int32 ret = NativeMethods.ion_port_create(ptr, key, type);
+            Int32 ret = NativeMethods.ion_port_create(ptr, key, type, dim);
             if (ret != 0) {
                 throw new InvalidOperationException();
             }
@@ -181,12 +181,12 @@ public class Ion
 
     public class Param
     {
-        private IntPtr obj;
+        public IntPtr obj;
 
         public Param(String key, String val)
         {
             IntPtr ptr = Marshal.AllocHGlobal(IntPtr.Size);
-            Int32 ret = NativeMethods.ion_param_create(ptr, key, type);
+            Int32 ret = NativeMethods.ion_param_create(ptr, key, val);
             if (ret != 0) {
                 throw new InvalidOperationException();
             }
@@ -196,7 +196,7 @@ public class Ion
 
         ~Param()
         {
-            if (obj != InPtr.Zero) {
+            if (obj != IntPtr.Zero) {
                 NativeMethods.ion_param_destroy(obj);
             }
         }
@@ -204,7 +204,12 @@ public class Ion
 
     public class Node
     {
-        private IntPtr obj;
+        public IntPtr obj;
+
+        public Node(IntPtr obj_)
+        {
+            obj = obj_;
+        }
 
         public Node()
         {
@@ -219,7 +224,7 @@ public class Ion
 
         ~Node()
         {
-            if (obj != InPtr.Zero) {
+            if (obj != IntPtr.Zero) {
                 NativeMethods.ion_node_destroy(obj);
             }
         }
@@ -232,10 +237,314 @@ public class Ion
                 if (ret != 0) {
                     throw new InvalidOperationException();
                 }
-                Port port(Marshal.ReadIntPtr(ptr));
+                var port = new Port(Marshal.ReadIntPtr(ptr));
                 Marshal.FreeHGlobal(ptr);
                 return port;
             }
+        }
+
+        public Node SetPort(params Port[] ports)
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(ports.Length * IntPtr.Size);
+            for (int i=0; i<ports.Length; ++i)
+            {
+                Marshal.WriteIntPtr(ptr + i * IntPtr.Size, ports[i].obj);
+            }
+            Int32 ret = NativeMethods.ion_node_set_port(obj, ptr, ports.Length);
+            if (ret != 0)
+            {
+                throw new InvalidCastException();
+            }
+            Marshal.FreeHGlobal(ptr);
+            return this;
+        }
+        public Node SetParam(params Param[] ps)
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(ps.Length * IntPtr.Size);
+            for (int i = 0; i < ps.Length; ++i)
+            {
+                Marshal.WriteIntPtr(ptr + i * IntPtr.Size, ps[i].obj);
+            }
+            Int32 ret = NativeMethods.ion_node_set_param(obj, ptr, ps.Length);
+            if (ret != 0)
+            {
+                throw new InvalidCastException();
+            }
+            Marshal.FreeHGlobal(ptr);
+            return this;
+        }
+    }
+
+    public class Builder
+    {
+        public IntPtr obj;
+
+        public Builder()
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(IntPtr.Size);
+            Int32 ret = NativeMethods.ion_builder_create(ptr);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+            obj = Marshal.ReadIntPtr(ptr);
+            Marshal.FreeHGlobal(ptr);
+        }
+
+        ~Builder()
+        {
+            if (obj != IntPtr.Zero)
+            {
+                NativeMethods.ion_builder_destroy(obj);
+            }
+        }
+
+        public Builder SetTarget(String target)
+        {
+            Int32 ret = NativeMethods.ion_builder_set_target(obj, target);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return this;
+        }
+
+        public Builder WithBBModule(String path)
+        {
+            Int32 ret = NativeMethods.ion_builder_with_bb_module(obj, path);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return this;
+        }
+
+        public Node Add(String key)
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(IntPtr.Size);
+            Int32 ret = NativeMethods.ion_builder_add_node(obj, key, ptr);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+            var node = new Node(Marshal.ReadIntPtr(ptr));
+            Marshal.FreeHGlobal(ptr);
+            return node;
+        }
+
+        public void Compile(String func_name, BuilderCompileOption option)
+        {
+            Int32 ret = NativeMethods.ion_builder_compile(obj, func_name, option);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        public void Save(String file_name)
+        {
+            Int32 ret = NativeMethods.ion_builder_save(obj, file_name);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        public void Load(String file_name)
+        {
+            Int32 ret = NativeMethods.ion_builder_load(obj, file_name);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Run(PortMap pm)
+        {
+            Int32 ret = NativeMethods.ion_builder_run(obj, pm.obj);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+    public class Buffer
+    {
+        public IntPtr obj;
+
+        public Buffer(Type type, params int[] sizes) {
+            IntPtr ptr = Marshal.AllocHGlobal(IntPtr.Size);
+            IntPtr sizes_ptr = Marshal.AllocHGlobal(sizeof(Int32) * sizes.Length);
+            for (int i=0; i<sizes.Length; ++i)
+            {
+                Marshal.WriteInt32(sizes_ptr + i * sizeof(Int32), sizes[i]);
+            }
+            Int32 ret = NativeMethods.ion_buffer_create(ptr, type, sizes_ptr, sizes.Length);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            obj = Marshal.ReadIntPtr(ptr);
+
+            Marshal.FreeHGlobal(sizes_ptr);
+            Marshal.FreeHGlobal(ptr);
+        }
+
+        ~Buffer()
+        {
+            if (obj != IntPtr.Zero)
+            {
+                NativeMethods.ion_builder_destroy(obj);
+            }
+        }
+
+        public void Write(byte[] data)
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(data.Length);
+            Marshal.Copy(data, 0, ptr, data.Length);
+            Int32 ret = NativeMethods.ion_buffer_write(obj, ptr, data.Length);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+            Marshal.FreeHGlobal(ptr);
+        }
+
+        public void Read(byte[] data)
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(data.Length);          
+            Int32 ret = NativeMethods.ion_buffer_read(obj, ptr, data.Length);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+            Marshal.Copy(ptr, data, 0, data.Length);
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+
+    public class PortMap
+    {
+        public IntPtr obj;
+
+        public PortMap()
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(IntPtr.Size);
+            Int32 ret = NativeMethods.ion_port_map_create(ptr);
+            if (ret != 0)
+            {
+                throw new InvalidOperationException();
+            }
+            obj = Marshal.ReadIntPtr(ptr);
+            Marshal.FreeHGlobal(ptr);  
+        }
+
+        ~PortMap()
+        {
+            if (obj != IntPtr.Zero) {            
+                Int32 ret = NativeMethods.ion_port_map_destroy(obj);      
+            }
+        }
+
+        public void Set(Port p, SByte v)
+        {
+            if (NativeMethods.ion_port_map_set_i8(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, Int16 v)
+        {
+            if (NativeMethods.ion_port_map_set_i16(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, Int32 v)
+        {
+            if (NativeMethods.ion_port_map_set_i32(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, Int64 v)
+        {
+            if (NativeMethods.ion_port_map_set_i64(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, Byte v)
+        {
+            if (NativeMethods.ion_port_map_set_u8(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, UInt16 v)
+        {
+            if (NativeMethods.ion_port_map_set_u16(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, UInt32 v)
+        {
+            if (NativeMethods.ion_port_map_set_u32(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, UInt64 v)
+        {
+            if (NativeMethods.ion_port_map_set_u64(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, float v)
+        {
+            if (NativeMethods.ion_port_map_set_f32(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, double v)
+        {
+            if (NativeMethods.ion_port_map_set_f64(obj, p.obj, v) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, Buffer buffer)
+        {
+            if (NativeMethods.ion_port_map_set_buffer(obj, p.obj, buffer.obj) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Set(Port p, Buffer[] buffers)
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(buffers.Length * IntPtr.Size);
+            for (int i = 0; i < buffers.Length; ++i)
+            {
+                Marshal.WriteIntPtr(ptr + i * IntPtr.Size, buffers[i].obj);
+            }
+            if (NativeMethods.ion_port_map_set_buffer_array(obj, p.obj, ptr, buffers.Length) != 0)
+            {
+                throw new InvalidOperationException();
+            }
+            Marshal.FreeHGlobal(ptr);
         }
     }
 }
--- test/Test.cs
+++ test/Test.cs
@@ -7,9 +7,72 @@ namespace test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PortTest1()
         {
-            Ion.Port port = new Ion.Port("test", new Ion.Type(Ion.TypeCode.Uint, 1, 1));
+            var t = new Ion.Type(Ion.TypeCode.Int, 32, 1);
+            var port = new Ion.Port("v", t, 0);
+
         }
+
+        [TestMethod]
+        public void ParamTest1()
+        {
+            var param = new Ion.Param("x", "1");
+        }
+
+        [TestMethod]
+        public void NodeTest1()
+        {
+            var node = new Ion.Node();
+        }
+
+        [TestMethod]
+        public void RunTest()
+        {
+            var t = new Ion.Type(Ion.TypeCode.Int, 32, 1);
+
+            var ip = new Ion.Port("input", t, 2);
+
+            var v41 = new Ion.Param("v", "41");
+
+            var b = new Ion.Builder();
+            b.SetTarget("host");
+            b.WithBBModule("ion-bb-test.dll");
+
+            var n = b.Add("test_inc_i32x2").SetPort(ip).SetParam(v41);
+
+            var pm = new Ion.PortMap();
+            
+            int[] sizes = { 4, 4 };
+            var ibuf = new Ion.Buffer(t, sizes);
+            var obuf = new Ion.Buffer(t, sizes);
+
+            Int32[] idata = new Int32[4 * 4];
+            Array.Fill(idata, 1);
+            
+            Int32[] odata = new Int32[4 * 4];
+            Array.Fill(odata, 0);
+
+            byte[] buf = new byte[4 * 4 * sizeof(Int32)];
+
+            Buffer.BlockCopy(idata, 0, buf, 0, buf.Length);
+            ibuf.Write(buf);
+            
+            Buffer.BlockCopy(odata, 0, buf, 0, buf.Length);
+            obuf.Write(buf);
+
+            pm.Set(ip, ibuf);
+            pm.Set(n["output"], obuf);
+            
+            b.Run(pm);
+
+            obuf.Read(buf);
+            Buffer.BlockCopy(buf, 0, odata, 0, buf.Length);
+
+            for (int i=0; i<4 * 4; ++i)
+            {
+                Assert.AreEqual(42, odata[i]);                          
+            }
+         }
     }
 }
